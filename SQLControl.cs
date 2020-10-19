8014300668c37389d895c217542b33012362db03
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-10-19 14:37:08 +0200

    Add project files.

---
+++ SQLControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageBlazor
+{
+    public class SQLControl
+    {
+        private static SqlConnection sqlConnection;
+
+        public static string CreateConnectionString()
+        {
+            SqlConnectionStringBuilder sqlCnt = new SqlConnectionStringBuilder();
+            sqlCnt["Server"] = @"BENJAMIN-ELIF-L\MSSQLSERVER02";
+            sqlCnt.InitialCatalog = "NetStorage";
+            sqlCnt.IntegratedSecurity = true;
+            return sqlCnt.ToString();
+        }
+
+        public static bool CreateConnection(string connectionString)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                connection.Close();
+                sqlConnection = connection;
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ WareItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageBlazor
+{
+    public class WareItem
+    {
+        public string Name { get; set; }
+        public string Information { get; set; }
+        public string Type { get; set; }
+        public string ID { get; set; }
+        public int Amount { get; set; }
+    }
+}
